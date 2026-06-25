b5daff54f695be7100b1489d0a78db7b9fc94d26
Jeevanj-2212 <jeevanjajekar@gmail.com>
2026-06-25 23:25:07 +0530

    chore: initial commit for Sentiment API gateway

---
+++ src/test/java/com/tavant/sentiment/sentiment_api/SentimentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tavant.sentiment.sentiment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentimentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
